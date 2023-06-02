3634db4f8cc0f0e3fb1d319d30f23b2533b38bf3
AMKS <arushisomani@berkeley.edu>
2023-06-02 12:51:08 -0700

    moving linear api call to sources

--- server/server.py
+++ server/server.py
@@ -47,20 +47,3 @@ async def root():
 
 @app.get("/linear")
 async def linear():
-    try:
-        linear_key = os.environ["LINEAR_API_KEY"]
-        headers = {
-            "Content-Type": "application/json",
-            "Authorization": linear_key
-        }
-
-        query = {
-            "query": "{ issues { nodes { id title } } }"
-        }
-
-        response = requests.post("https://api.linear.app/graphql", headers=headers, json=query)
-        print(response.json())
-        return response.json()
-    except Exception as ex:
-        print(ex)
-        return {"error": "yes"}
