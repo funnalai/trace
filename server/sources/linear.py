705284982d78956ed27025feff6c72aa559b35d3
AMKS <arushisomani@berkeley.edu>
2023-06-02 13:03:35 -0700

    linear data fetching

--- server/sources/linear.py
+++ server/sources/linear.py
@@ -1,21 +1,42 @@
 import os
 import requests
+import json
+
+queryStr = """
+{
+    issues {
+        nodes {
+            id
+            title
+            project {
+                id
+                name
+            }
+            description
+            createdAt
+            assignee {
+                id
+                name
+            }
+        }
+    }
+}
+"""
 
 def get_linear_data():
     try:
-        linear_key = os.environ["LINEAR_API_KEY"]
+        linear_key = os.getenv("LINEAR_API_KEY")
+        if not linear_key:
+            return {"error": "No Linear API key found"}
+
         headers = {
             "Content-Type": "application/json",
             "Authorization": linear_key
         }
-
-        query = {
-            "query": "{ issues { nodes { id title } } }"
-        }
-
+        query = { "query": queryStr }
         response = requests.post("https://api.linear.app/graphql", headers=headers, json=query)
-        print(response.json())
+
         return response.json()
+
     except Exception as ex:
-        print(ex)
-        return {"error": "yes"}
+        return {"error": json.dumps(ex)}
