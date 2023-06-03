ece646e4567f230a89f7f7014543b0417084375b
amirgamil <amir.amgad01@gmail.com>
2023-06-03 10:10:48 -0700

    chore: clean up ui

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,25 +2,28 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import { getEmployees } from "../utils/api";
 import { useQuery } from "@tanstack/react-query";
+import styled from "styled-components";
+import { EmployeeCard } from "../components/Employee";
 
 const inter = Inter({ subsets: ["latin"] });
+
+const Container = styled.main``;
 
 export default function Home() {
     const { data: employees, isLoading } = useQuery<User[]>({ queryKey: ["employees"], queryFn: getEmployees });
     console.log(employees);
     return (
-        <main className={`flex min-h-screen flex-col items-center justify-center p-24 ${inter.className}`}>
-            <h2 className="text-xl">Funnal</h2>
+        <Container className={`flex min-h-screen flex-col items-center justify-center p-24 ${inter.className}`}>
+            <h2 className="text-xl">
+                <strong>Funnal</strong>
+            </h2>
+            <div className="py-2"></div>
             <h3>My employees</h3>
             {isLoading || !employees ? (
                 <p>loading...</p>
             ) : (
-                employees.map((employee, index) => (
-                    <div style={{ margin: 0, padding: 0 }} key={index}>
-                        <p>{employee.name}</p>
-                    </div>
-                ))
+                employees.map((employee, index) => <EmployeeCard employee={employee} key={index} />)
             )}
-        </main>
+        </Container>
     );
 }
