8f161488e0417774fb2500f3fac2aa4f8e3d277d
will-hath <willhath@mit.edu>
2023-06-03 12:31:33 -0700

    visualize convos

--- server/utils/classifier.py
+++ server/utils/classifier.py
@@ -5,7 +5,7 @@ from langchain import PromptTemplate
 import json
 import os
 from dotenv import load_dotenv
-
+from langchain import Document, load_summarize_chain
 load_dotenv()
 
 
@@ -19,3 +19,20 @@ def get_conv_classification(convStr, projNames):
     response = llm(prompt)
     return response
 
+def get_natural_convs_title(summaries):
+    """
+    Create few-word, topic-based summarization of a list of conversation summaries
+    """
+    llm = OpenAI(openai_api_key=os.getenv("OPENAI_API_KEY"), temperature=0)
+    docs = [Document(page_content=text) for text in summaries]
+    prompt = """
+    Write a title for the following summaries of conversations
+    "{text}"
+    TITLE:
+    """
+    prompt_template = PromptTemplate(template=prompt, input_variables=["text"])
+
+    summarize_chain = load_summarize_chain(
+        llm, chain_type="stuff", prompt=prompt_template)
+    title = summarize_chain.run(docs)
+    return title
