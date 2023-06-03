c1add544137fab4dafae37ec2204d964fee0beb8
yujinglun <yujinglun12@gmail.com>
2023-06-03 12:40:25 -0700

    update classifier

--- server/utils/classifier.py
+++ server/utils/classifier.py
@@ -13,8 +13,8 @@ def get_conv_classification(convStr, projNames):
     """
     Given a conversation, get the project it belongs to
     """
-    llm = OpenAI(openai_api_key=os.getenv("OPENAI_API_KEY"), temperature=0, max_tokens=5)
-    prompt = f"This conversation is about one of these projects {', '.join(projNames)}. Select which one. Return None if no project is related."
+    llm = OpenAI(openai_api_key=os.getenv("OPENAI_API_KEY"), temperature=0.7, max_tokens=5)
+    prompt = f"{convStr}\n This conversation is about one of these projects {', '.join(projNames)}. Select which project matches the conversation best. Return None if no project is related."
     # make llm call
     response = llm(prompt)
     return response
