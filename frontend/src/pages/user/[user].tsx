fafce87e608f932e725bebb3a78df12df8ceeed7
amirgamil <amir.amgad01@gmail.com>
2023-06-04 10:11:40 -0700

    feat: add loading indicator and small textarea fixes

--- frontend/src/pages/user/[user].tsx
+++ frontend/src/pages/user/[user].tsx
@@ -6,6 +6,7 @@ import { useRouter } from "next/router";
 import { useQuery } from "@tanstack/react-query";
 import { getChatResponse, getEmployee } from "../../utils/api";
 import { EmployeeDetail } from "../../components/EmployeeDetail";
+import BounceLoader from "react-spinners/BounceLoader";
 import Link from "next/link";
 
 const jbm = JetBrains_Mono({ subsets: ["latin"] });
@@ -31,48 +32,67 @@ export default function User() {
                 </Link>
             </div>
             <Container className={`flex min-h-screen flex-col items-center justify-center p-24 ${jbm.className}`}>
-            <h2 className="text-xl"></h2>
-            {isLoading || !userData ? <p>loading...</p> : <EmployeeDetail employee={userData}></EmployeeDetail>}
+                <h2 className="text-xl"></h2>
+                {isLoading || !userData ? <p>loading...</p> : <EmployeeDetail employee={userData}></EmployeeDetail>}
             </Container>
             <ChatBox userId={userId as string} />
         </MainContainer>
     );
 }
 
-function ChatBox({ userId,  }: { userId: string }) {
+function ChatBox({ userId }: { userId: string }) {
     const [input, setInput] = useState("");
     const [output, setOutput] = useState("");
     const [history, setHistory] = useState<string[]>([]);
+    const [isLoading, setIsLoading] = useState(false);
 
     // Make call to getChatResponse on submitHandler
     const submitHandler = () => {
         setHistory([...history, input]);
         setInput("");
+        setIsLoading(true);
         getChatResponse(userId, input).then((res) => {
             const data = res["summary"];
             console.log("data", data);
             setOutput(data);
+            setIsLoading(false);
         });
     };
     // Stick chatbox to bottom right of page, set background to gray
     return (
         // Set max width to 400px
-        <Container className={`fixed bottom-5 right-5 p-4 border-2 border-gray-300 bg-gray-100 rounded-md ${jbm.className} shadow-md w-96`}>
-            {output && <div className="py-2 border-2 p-2 mb-4 ">
-                <h3 className="font-bold">Most Relevant Conversation → </h3>
-                <TextDisplay text={output} />
-            </div>}
+        <Container
+            className={`fixed bottom-5 right-5 p-4 border-2 border-gray-300 bg-gray-100 rounded-md ${jbm.className} shadow-md w-96`}
+        >
             <h1 className={`font-bold pb-2`}>Ask me about anything</h1>
-            <div className="flex flex-col items-start">
-            <textarea
-                value={input}
-                onChange={(e) => {
-                    setInput(e.target.value);
-                }}
-            />
-            <button onClick={submitHandler} className="bg-blue-400 text-white p-1 mt-2 rounded-md">Send</button>
+            <div className="flex flex-col items-center justify-center">
+                <textarea
+                    style={{
+                        outline: "none",
+                        padding: "0.5rem",
+                        fontSize: "0.75rem",
+                        resize: "none",
+                        border: "2px solid #cbd5e1",
+                        borderRadius: "0.5rem",
+                    }}
+                    className="w-full"
+                    value={input}
+                    onChange={(e) => {
+                        setInput(e.target.value);
+                    }}
+                />
+                <button onClick={submitHandler} className="bg-blue-400 text-white p-1 mt-2 rounded-md">
+                    Send
+                </button>
+                <div className="py-2"></div>
+                {isLoading && <BounceLoader color={"#60a5fa"} size={30} />}
+                {output && !isLoading && (
+                    <div className="py-2 border-2 p-2 mb-4 ">
+                        <h3 className="font-bold">Most Relevant Conversation → </h3>
+                        <TextDisplay text={output} />
+                    </div>
+                )}
             </div>
-
         </Container>
     );
 }
@@ -87,14 +107,18 @@ function TextDisplay({ text }: { text: string }) {
         return (
             <div>
                 <p className="text-sm">{text}</p>
-                <button onClick={() => setShowMore(false)} className=" text-sm text-blue-600">Show Less</button>
+                <button onClick={() => setShowMore(false)} className=" text-sm text-blue-600">
+                    Show Less
+                </button>
             </div>
         );
     }
     return (
         <div>
             <p className="text-sm">{text.slice(0, 100)}...</p>
-            <button onClick={() => setShowMore(true)} className=" text-sm text-blue-600">Show More</button>
+            <button onClick={() => setShowMore(true)} className=" text-sm text-blue-600">
+                Show More
+            </button>
         </div>
     );
 }
