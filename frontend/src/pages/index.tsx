0c35da77870c4033d9d5f3017010a9a7ff40212a
AMKS <arushisomani@berkeley.edu>
2023-06-04 11:14:49 -0700

    funnal -> trace

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
     return (
         <Container className={`flex min-h-screen flex-col justify-center p-24 pt-4 ${jbm.className}`}>
             <h2 className="text-4xl font-bold pb-2 border-b-2 border-gray-600">
-                Funnal<span className="text-blue-500">.ai</span>
+                Trace<span className="text-blue-500">.ai</span>
             </h2>
             <div className="py-2"></div>
             <h3 className="text-2xl font-bold text-gray-600 pb-2">My Employees</h3>
